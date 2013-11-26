a1061ecb68e86579cd64e83a8656f56100cf044e
Bruno Veiga <bsv259@cs.utexas.edu>
2013-11-25 18:14:09 -0600

    function to display delete button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-$(document).ready(function() {
-	function enableEditing(pageid) {
-		var page = document.getElementById(pageid);
+// $(document).ready(function() {
 
-	});
-});
+function enableEditing() {
+	var lhs = document.getElementsByClassName('lhs');
+	var rhs = document.getElementsByClassName('rhs');
+	var i;
+
+	if (lhs[0].style.display === "none") {
+		for(i = 0; i<lhs.length; i++) {
+			lhs[i].style.display = "block";
+		}
+
+		for(i = 0; i<rhs.length; i++) {
+			rhs[i].style.display = "none";
+		}
+	}
+
+	else {
+		for(i = 0; i<lhs.length; i++) {
+			lhs[i].style.display = "none";
+		}
+
+		for(i = 0; i<rhs.length; i++) {
+			rhs[i].style.display = "block";
+		}
+	}
+}
+
+// });
